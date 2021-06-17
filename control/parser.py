f85bdb5b60c07119598bc0c4e79cc5d69f25efba
Zed Jensen <zed@zayda.net>
2021-06-17 00:06:13 -0600

    Update song format

--- control/parser.py
+++ control/parser.py
@@ -2,19 +2,16 @@ def import_notes(lines):
     result = []
     for line in lines:
         parts = line.strip().split(' ')
-        time_slot = []
-        for i in range(len(parts))[::2]:
-            time_slot.append({
-                'note': parts[i],
-                'duration': float(parts[i+1])
+        result.append(
+            {
+                'notes': parts[:-1],
+                'duration': float(parts[-1])
             })
-        result.append(time_slot)
     return result
 
 
 def export_notes(song):
     result = ''
     for time_slot in song:
-        time_slot = list(map(lambda note: f"{note['note']} {note['duration']}", time_slot))
-        result += ' '.join(time_slot) + '\n'
+        result += ' '.join(time_slot['notes']) + f" {time_slot['duration']}\n"
     return result
