d7d766516ba0a5136cda7cfa9e523def4d43e7fa
Zed Jensen <zed@endpoint.com>
2021-06-18 18:34:37 -0600

    Add built first version of app, serve static files

--- control/urls.py
+++ control/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path
+from django.conf import settings
+
+from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
@@ -6,4 +9,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('all_songs', views.all_songs, name='all_songs'),
     path('song/<int:pk>/play', views.play_song, name='play_song'),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
